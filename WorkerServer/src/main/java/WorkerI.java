c46d6e4ef69d47dd10e9f698cc0748fc07b2ffec
JuanF2019 <juan.martinez27@u.icesi.edu.co>
2023-12-05 13:05:24 -0500

    ForkJoin pull shut down

--- WorkerServer/src/main/java/WorkerI.java
+++ WorkerServer/src/main/java/WorkerI.java
@@ -215,6 +215,7 @@ public class WorkerI extends ThreadPoolExecutor implements AppInterface.Worker {
         isRunning = false;
         shutdown();
         session.disconnect();
+        fjPool.shutdown();
         removeTemporalFiles();
     }
 
